7c357d0522fe862ce2ae4237ad2c12d1a98a9735
Keith Schneider <37637461+kschneider0422@users.noreply.github.com>
2025-09-26 08:07:39 -0600

    Add files via upload

---
+++ scripts/lookup.py
@@ -0,0 +1,425 @@
+team_names = [
+    "Arizona Diamondbacks",
+    "Atlanta Braves",
+    "Baltimore Orioles",
+    "Boston Red Sox",
+    "Chicago Cubs",
+    "Chicago White Sox",
+    "Cincinnati Reds",
+    "Cleveland Guardians",
+    "Colorado Rockies",
+    "Detroit Tigers",
+    "Houston Astros",
+    "Kansas City Royals",
+    "Los Angeles Angels",
+    "Los Angeles Dodgers",
+    "Miami Marlins",
+    "Milwaukee Brewers",
+    "Minnesota Twins",
+    "New York Mets",
+    "New York Yankees",
+    "Oakland Athletics",
+    "Philadelphia Phillies",
+    "Pittsburgh Pirates",
+    "San Diego Padres",
+    "San Francisco Giants",
+    "Seattle Mariners",
+    "St. Louis Cardinals",
+    "Tampa Bay Rays",
+    "Texas Rangers",
+    "Toronto Blue Jays",
+    "Washington Nationals"
+]
+
+
+team_codes = [
+    "ARI",
+    "ATH",
+    "ATL",
+    "BAL",
+    "BOS",
+    "CHC",
+    "CHW",
+    "CIN",
+    "CLE",
+    "COL",
+    "DET",
+    "HOU",
+    "KCR",
+    "LAA",
+    "LAD",
+    "MIA",
+    "MIL",
+    "MIN",
+    "NYM",
+    "NYY",
+    "OAK",
+    "PHI",
+    "PIT",
+    "SDP",
+    "SEA",
+    "SFG",
+    "STL",
+    "TB",
+    "TEX",
+    "TOR",
+    "WSH"
+]
+
+
+team_lookup = {
+    "ARI": "Arizona Diamondbacks",
+    "ATH": "Athletics",
+    "ATL": "Atlanta Braves",
+    "BAL": "Baltimore Orioles",
+    "BOS": "Boston Red Sox",
+    "CHC": "Chicago Cubs",
+    "CHW": "Chicago White Sox",
+    "CIN": "Cincinnati Reds",
+    "CLE": "Cleveland Guardians",
+    "COL": "Colorado Rockies",
+    "DET": "Detroit Tigers",
+    "HOU": "Houston Astros",
+    "KCR": "Kansas City Royals",
+    "LAA": "Los Angeles Angels",
+    "LAD": "Los Angeles Dodgers",
+    "MIA": "Miami Marlins",
+    "MIL": "Milwaukee Brewers",
+    "MIN": "Minnesota Twins",
+    "NYM": "New York Mets",
+    "NYY": "New York Yankees",
+    "OAK": "Oakland Athletics",
+    "PHI": "Philadelphia Phillies",
+    "PIT": "Pittsburgh Pirates",
+    "SDP": "San Diego Padres",
+    "SFG": "San Francisco Giants",
+    "SEA": "Seattle Mariners",
+    "STL": "St. Louis Cardinals",
+    "TB": "Tampa Bay Rays",
+    "TEX": "Texas Rangers",
+    "TOR": "Toronto Blue Jays",
+    "WSH": "Washington Nationals",
+    "Arizona Diamondbacks": "ARI",
+    "Athletics": "ATH",
+    "Atlanta Braves": "ATL",
+    "Baltimore Orioles": "BAL",
+    "Boston Red Sox": "BOS",
+    "Chicago Cubs": "CHC",
+    "Chicago White Sox": "CHW",
+    "Cincinnati Reds": "CIN",
+    "Cleveland Guardians": "CLE",
+    "Colorado Rockies": "COL",
+    "Detroit Tigers": "DET",
+    "Houston Astros": "HOU",
+    "Kansas City Royals": "KCR",
+    "Los Angeles Angels": "LAA",
+    "Los Angeles Dodgers": "LAD",
+    "Miami Marlins": "MIA",
+    "Milwaukee Brewers": "MIL",
+    "Minnesota Twins": "MIN",
+    "New York Mets": "NYM",
+    "New York Yankees": "NYY",
+    "Oakland Athletics": "OAK",
+    "Philadelphia Phillies": "PHI",
+    "Pittsburgh Pirates": "PIT",
+    "San Diego Padres": "SDP",
+    "San Francisco Giants": "SFG",
+    "Seattle Mariners": "SEA",
+    "St. Louis Cardinals": "STL",
+    "Tampa Bay Rays": "TB",
+    "Texas Rangers": "TEX",
+    "Toronto Blue Jays": "TOR",
+    "Washington Nationals": "WSH"
+}
+
+
+team_lookup_l2s = {
+    "Arizona Diamondbacks": "ARI",
+    "Athletics": "ATH",
+    "Atlanta Braves": "ATL",
+    "Baltimore Orioles": "BAL",
+    "Boston Red Sox": "BOS",
+    "Chicago Cubs": "CHC",
+    "Chicago White Sox": "CHW",
+    "Cincinnati Reds": "CIN",
+    "Cleveland Guardians": "CLE",
+    "Cleveland Indians": "CLE",
+    "Colorado Rockies": "COL",
+    "Detroit Tigers": "DET",
+    "Houston Astros": "HOU",
+    "Kansas City Royals": "KCR",
+    "Los Angeles Angels": "LAA",
+    "Los Angeles Dodgers": "LAD",
+    "Miami Marlins": "MIA",
+    "Milwaukee Brewers": "MIL",
+    "Minnesota Twins": "MIN",
+    "New York Mets": "NYM",
+    "New York Yankees": "NYY",
+    "Oakland Athletics": "ATH",
+    "Philadelphia Phillies": "PHI",
+    "Pittsburgh Pirates": "PIT",
+    "San Diego Padres": "SDP",
+    "San Francisco Giants": "SFG",
+    "Seattle Mariners": "SEA",
+    "St. Louis Cardinals": "STL",
+    "Tampa Bay Rays": "TB",
+    "Texas Rangers": "TEX",
+    "Toronto Blue Jays": "TOR",
+    "Washington Nationals": "WSH"
+}
+
+
+team_colors = {
+    "Baltimore Orioles": {
+        "ColorA": "#DF4601",
+        "ColorB": "#000000"
+    },
+    "Boston Red Sox": {
+        "ColorA": "#BD3039",
+        "ColorB": "#0C2340"
+    },
+    "New York Yankees": {
+        "ColorA": "#0C2340",
+        "ColorB": "#C4CED3"
+    },
+    "Tampa Bay Rays": {
+        "ColorA": "#8FBCE6",
+        "ColorB": "#092C5C"
+    },
+    "Toronto Blue Jays": {
+        "ColorA": "#134A8E",
+        "ColorB": "#E8291C"
+    },
+    "Chicago White Sox": {
+        "ColorA": "#27251F",
+        "ColorB": "#FFFFFF"
+    },
+    "Cleveland Guardians": {
+        "ColorA": "#E50022",
+        "ColorB": "#00385D"
+    },
+    "Detroit Tigers": {
+        "ColorA": "#0C2340",
+        "ColorB": "#FA4616"
+    },
+    "Kansas City Royals": {
+        "ColorA": "#004687",
+        "ColorB": "#BD9B60"
+    },
+    "Minnesota Twins": {
+        "ColorA": "#D31145",
+        "ColorB": "#002B5C"
+    },
+    "Houston Astros": {
+        "ColorA": "#EB6E1F",
+        "ColorB": "#002D62"
+    },
+    "Los Angeles Angels": {
+        "ColorA": "#BA0021",
+        "ColorB": "#003263"
+    },
+    "Oakland Athletics": {
+        "ColorA": "#003831",
+        "ColorB": "#EFB21E"
+    },
+    "Athletics": {
+        "ColorA": "#003831",
+        "ColorB": "#EFB21E"
+    },
+    "Seattle Mariners": {
+        "ColorA": "#005C5C",
+        "ColorB": "#C4CED4"
+    },
+    "Texas Rangers": {
+        "ColorA": "#003278",
+        "ColorB": "#C0111F"
+    },
+    "Atlanta Braves": {
+        "ColorA": "#CE1141",
+        "ColorB": "#13274F"
+    },
+    "Miami Marlins": {
+        "ColorA": "#00A3E0",
+        "ColorB": "#41748D"
+    },
+    "New York Mets": {
+        "ColorA": "#FF5910",
+        "ColorB": "#002D72"
+    },
+    "Philadelphia Phillies": {
+        "ColorA": "#6F263D",
+        "ColorB": "#6BACE4"
+    },
+    "Washington Nationals": {
+        "ColorA": "#AB0003",
+        "ColorB": "#FFFFFF"
+    },
+    "Chicago Cubs": {
+        "ColorA": "#CC3433",
+        "ColorB": "#0E3386"
+    },
+    "Cincinnati Reds": {
+        "ColorA": "#C6011F",
+        "ColorB": "#FFFFFF"
+    },
+    "Milwaukee Brewers": {
+        "ColorA": "#002B5C",
+        "ColorB": "#FFC52F"
+    },
+    "Pittsburgh Pirates": {
+        "ColorA": "#FDB827",
+        "ColorB": "#27251F"
+    },
+    "St. Louis Cardinals": {
+        "ColorA": "#C41E3A",
+        "ColorB": "#77AEE3"
+    },
+    "Arizona Diamondbacks": {
+        "ColorA": "#A71930",
+        "ColorB": "#E3D4AD"
+    },
+    "Colorado Rockies": {
+        "ColorA": "#333366",
+        "ColorB": "#C4CED4"
+    },
+    "Los Angeles Dodgers": {
+        "ColorA": "#005A9C",
+        "ColorB": "#FFFFFF"
+    },
+    "San Diego Padres": {
+        "ColorA": "#2F241D",
+        "ColorB": "#FFC425"
+    },
+    "San Francisco Giants": {
+        "ColorA": "#FD5A1E",
+        "ColorB": "#27251F"
+    }
+}
+
+
+divisions = {
+    "AL East": [
+        "BAL",
+        "BOS",
+        "NYY",
+        "TB",
+        "TOR"
+    ],
+    "AL Central": [
+        "CHW",
+        "CLE",
+        "DET",
+        "KCR",
+        "MIN"
+    ],
+    "AL West": [
+        "LAA",
+        "HOU",
+        "OAK",
+        "SEA",
+        "TEX",
+        "ATH"
+    ],
+    "NL East": [
+        "ATL",
+        "MIA",
+        "NYM",
+        "PHI",
+        "WSH"
+    ],
+    "NL Central": [
+        "CHC",
+        "CIN",
+        "MIL",
+        "PIT",
+        "STL"
+    ],
+    "NL West": [
+        "ARI",
+        "COL",
+        "LAD",
+        "SDP",
+        "SFG"
+    ]
+}
+
+current_divisions = {
+    "AL East": [
+        "BAL",
+        "BOS",
+        "NYY",
+        "TB",
+        "TOR"
+    ],
+    "AL Central": [
+        "CHW",
+        "CLE",
+        "DET",
+        "KCR",
+        "MIN"
+    ],
+    "AL West": [
+        "LAA",
+        "HOU",
+        "SEA",
+        "TEX",
+        "ATH"
+    ],
+    "NL East": [
+        "ATL",
+        "MIA",
+        "NYM",
+        "PHI",
+        "WSH"
+    ],
+    "NL Central": [
+        "CHC",
+        "CIN",
+        "MIL",
+        "PIT",
+        "STL"
+    ],
+    "NL West": [
+        "ARI",
+        "COL",
+        "LAD",
+        "SDP",
+        "SFG"
+    ]
+}
+
+
+logos = {
+    "Tampa Bay Rays": "logos/tbr.png",
+    "Baltimore Orioles": "logos/bal.png",
+    "Toronto Blue Jays": "logos/tor.png",
+    "New York Yankees": "logos/nyy.png",
+    "Boston Red Sox": "logos/bos.png",
+    "Minnesota Twins": "logos/min.png",
+    "Cleveland Guardians": "logos/cle.png",
+    "Cleveland Indians": "logos/cle.png",
+    "Detroit Tigers": "logos/det.png",
+    "Chicago White Sox": "logos/chw.png",
+    "Kansas City Royals": "logos/kcr.png",
+    "Texas Rangers": "logos/tex.png",
+    "Houston Astros": "logos/hou.png",
+    "Los Angeles Angels": "logos/laa.png",
+    "Seattle Mariners": "logos/sea.png",
+    "Oakland Athletics": "logos/oak.png",
+    "Athletics": "logos/oak.png",
+    "New York Mets": "logos/nym.png",
+    "Atlanta Braves": "logos/atl.png",
+    "Miami Marlins": "logos/mia.png",
+    "Philadelphia Phillies": "logos/phi.png",
+    "Washington Nationals": "logos/wsh.png",
+    "Pittsburgh Pirates": "logos/pit.png",
+    "Milwaukee Brewers": "logos/mil.png",
+    "Chicago Cubs": "logos/chc.png",
+    "St. Louis Cardinals": "logos/stl.png",
+    "Cincinnati Reds": "logos/cin.png",
+    "Arizona Diamondbacks": "logos/ari.png",
+    "Los Angeles Dodgers": "logos/lad.png",
+    "San Diego Padres": "logos/sdp.png",
+    "San Francisco Giants": "logos/sfg.png",
+    "Colorado Rockies": "logos/col.png"
+}
